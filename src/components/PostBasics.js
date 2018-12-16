6c1015fe534a02c2fbc1951f77b7061b8a86454e
Dain M. Blodorn Kim <dain@db13.us>
2018-12-16 13:26:32 -0800

    comment out head component

--- src/components/PostBasics.js
+++ src/components/PostBasics.js
@@ -2,6 +2,6 @@ import React, { Fragment } from 'react'
 import Head from './utils/Head'
 
 export default (props) => 
-  <Fragment>
-    <Head title={props.data.title} description={props.data.short_description}/>
+  <Fragment>{/*
+    <Head title={props.data.title} description={props.data.short_description}/>*/}
   </Fragment>
